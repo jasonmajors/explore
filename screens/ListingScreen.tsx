124a9231b29966eeab049f903ea06acc60fefbff
Jason Majors <jasonrmajors@gmail.com>
2019-08-27 02:02:45 -0700

    Refactor listing screen

--- screens/ListingScreen.tsx
+++ screens/ListingScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, ScrollView, Dimensions, Button } from "react-native";
+import { ScrollView, Dimensions, Button } from "react-native";
 import { HuntListing } from "../components/HuntListing";
 
 export class ListingScreen extends React.Component<any, any> {
@@ -7,11 +7,28 @@ export class ListingScreen extends React.Component<any, any> {
     title: "Hunts",
     headerRight: (
       <Button
-        onPress={() => alert('This is a button!')}
+        onPress={ () => alert('This is a button!') }
         title="Info"
         color="#fff"
       />
     ),
+  }
+  // TODO: Temp until setup firebase
+  listings : Array<any> = [{
+    title: "Naples",
+    description: "Hello it's naples omg so pretty. Enjoy a nice stroll around Naples and find some dope shit. It'll be so fun!",
+    image: "https://s3.amazonaws.com/zumpermedia/blog/wp-content/uploads/2018/12/06142608/iStock-1002431174.jpg",
+    },
+    {
+      title: "Bixby Knolls",
+      description: "Hello it's bixby omg so pretty. Enjoy a nice stroll around Bix and find some dope shit. It'll be so fun!",
+      image: "https://lbpost.com/hi-lo/wp-content/uploads/2019/02/Bixby-Knolls-.jpg",
+    }
+  ]
+
+  getHunts() : Array<any> {
+    // TODO: Get the data from the server
+    return this.listings
   }
 
   goToNext() : void {
@@ -21,23 +38,26 @@ export class ListingScreen extends React.Component<any, any> {
   }
 
   render() {
+    const hunts = []
+
+    this.getHunts().forEach((hunt, index) => {
+      hunts.push(
+        <HuntListing
+          key={ index }
+          title={ hunt.title }
+          description={ hunt.description }
+          image={ hunt.image }
+          nextHunt={ () => this.goToNext() }
+        />
+      )
+    })
+
     return (
       <ScrollView
         ref='scrollView'
         pagingEnabled
       >
-        <HuntListing
-          title="Naples"
-          description="Hello it's naples omg so pretty. Enjoy a nice stroll around Naples and find some dope shit. It'll be so fun!"
-          image="https://s3.amazonaws.com/zumpermedia/blog/wp-content/uploads/2018/12/06142608/iStock-1002431174.jpg"
-          nextHunt={() => this.goToNext()}
-        />
-        <HuntListing
-          title="Bixby Knolls"
-          description="Hello it's bixby omg so pretty. Enjoy a nice stroll around Bix and find some dope shit. It'll be so fun!"
-          image="https://lbpost.com/hi-lo/wp-content/uploads/2019/02/Bixby-Knolls-.jpg"
-          nextHunt={() => this.goToNext()} // TODO: Wont pass in a pagination prop if it's the last item in the array
-        />
+        { hunts }
       </ScrollView>
     );
   }
--- utils/strings.js
+++ utils/strings.js
@@ -1,4 +1,4 @@
 export default {
-  huntIndexCTA: 'See Details',
-  
+  huntIndexCTA: 'Available',
+
 }
