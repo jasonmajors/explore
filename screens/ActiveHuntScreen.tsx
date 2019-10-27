671b7fbf6687cc0926193a910d6e4bffd86052d2
Jason Majors <jasonrmajors@gmail.com>
2019-10-26 20:23:54 -0700

    WIP - loading current step in active hunt

--- screens/ActiveHuntScreen.tsx
+++ screens/ActiveHuntScreen.tsx
@@ -1,22 +1,63 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { UserContext } from '../context/UserContext';
+import { db } from '../utils/firebase';
+import { DocumentReference } from '@firebase/firestore-types'
 
-export class ActiveHuntScreen extends React.Component {
+export class ActiveHuntScreen extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      currentNode: null,
+    }
+  }
+
   static navigationOptions = {
     header: null
   }
 
-  static contextType = UserContext
+  static contextType: React.Context<any> = UserContext
 
-  componentDidMount() {
+  componentDidMount(): void {
     console.log('we have a hunt', this.context)
+    this.setCurrentHuntStatus()
+  }
+
+  async setCurrentHuntStatus(): Promise<void> {
+    const { activeHuntPivotId } = this.context
+
+    const activeHunt: DocumentReference = await this.fetchActiveHunt(activeHuntPivotId)
+
+    activeHunt.onSnapshot(currentHunt => {
+      const { currentNode, huntId } = currentHunt.data()
+      db.collection('hunts').doc(huntId)
+        .get()
+        .then(hunt => {
+          if (hunt.exists) {
+            const { title } = hunt.data()
+
+            this.setState({ title })
+            this.setState({ currentNode })
+            console.log('currently on step: ', currentNode)
+          } else {
+            // no hunt? redirect back to App I guess...
+          }
+        })
+    }, error => console.log(error))
+  }
+
+  async fetchActiveHunt(activeHuntPivotId): Promise<firebase.firestore.DocumentReference> {
+    return await db.collection('hunts_teams_users').doc(activeHuntPivotId)
   }
 
   render() {
+    const { title, currentNode } = this.state
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Hunt!</Text>
+        <Text>{ title }</Text>
+        <Text>{ currentNode }</Text>
       </View>
     );
   }
